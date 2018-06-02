cf3de97f1c4e66996507994ddeb705d4545e416c
vincentcheny <31912414+vincentcheny@users.noreply.github.com>
2018-06-02 15:40:49 +0800

    repair the SQL statement

---
+++ GECiv5TechTree (v 2.13)/SQL/GenericFunctionalities.sql
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------
+--	GENERAL UPDATES
+-----------------------------------------------------------------------
+
+UPDATE Buildings SET PrereqTech = 'TECH_MASONRY' WHERE PrereqTech = 'TECH_POTTERY';
+UPDATE Buildings SET PrereqTech = 'TECH_ARITHMETIC' WHERE PrereqTech = 'TECH_CALENDAR';
+UPDATE Buildings SET PrereqTech = 'TECH_IRON_WORKING' WHERE PrereqTech = 'TECH_CONSTRUCTION';
+UPDATE Buildings SET PrereqTech = 'TECH_ASSOCIATIVE_THINKING' WHERE PrereqTech = 'TECH_PHILOSOPHY';
+UPDATE Buildings SET PrereqTech = 'TECH_METAL_CASTING' WHERE PrereqTech = 'TECH_ENGINEERING';
+UPDATE Buildings SET PrereqTech = 'TECH_MYSTICISM' WHERE PrereqTech = 'TECH_ARCHERY';
+UPDATE Buildings SET PrereqTech = 'TECH_SCIENTIFIC_METHOD' WHERE PrereqTech = 'TECH_PHYSICS';
+
+UPDATE Units SET PrereqTech = 'TECH_SCIENTIFIC_METHOD' WHERE Type = 'UNIT_TREBUCHET';
+UPDATE Units SET PrereqTech = 'TECH_IRON_WORKING' WHERE PrereqTech = 'TECH_CONSTRUCTION';
